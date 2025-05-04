938b46433c4df6c03a4de75c898c26ef550c636d
brbr5252 <brbrhd423@gmail.com>
2025-05-04 14:37:45 +0300

    s2

--- test-app/src/components/pageHeaderContent/index.jsx
+++ test-app/src/components/pageHeaderContent/index.jsx
@@ -6,7 +6,6 @@ const PageHeaderContent = (props) => {
     return (
         <div className="wrapper">
             <h2>{headerText}</h2>
-            <span>{icon}</span>
         </div>
     );
 };
